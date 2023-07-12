6268ffbc4197d30af20bdfa25c9ef711ba18dcfa
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-12 18:30:34 -0300

    Update 3-1.js

--- 3-1.js
+++ 3-1.js
@@ -98,13 +98,89 @@ db.usuarios.insertMany([{
   }]);
   
 
-
+// Find
 db.usuarios.find({});
 db.usuarios.find({"nome": "João"});
 db.usuarios.findOne({"nome": "João"});
 db.usuarios.findOneAndUpdate({ nome: "João" }, { $set: { idade: 26 } });
 db.usuarios.findOneAndDelete({ nome: "João" });
-db.usuarios.findAndModify({
-    query: { nome: "Pedro" },
-    update: { $set: { idade: 26 } }
-  });
+
+// Update
+db.usuarios.updateOne(
+  { nome: "João" },
+  { $set: { idade: 26 } }
+);
+
+db.usuarios.updateMany(
+  { cidade: "São Paulo" },
+  { $set: { estado: "SP" } }
+);
+db.usuarios.replaceOne(
+  { nome: "João" },
+  {
+    nome: "John",
+    idade: 27,
+    cidade: "São Paulo",
+    estado: "SP",
+    endereco: {
+      rua: "Avenida Principal",
+      numero: 123
+    }
+  }
+);
+
+// Update Operadores
+// Usando o operador $set para definir o valor de um campo específico
+db.usuarios.updateOne({ nome: "João" }, { $set: { idade: 26 } });
+
+// Usando o operador $inc para incrementar o valor de um campo numérico
+db.usuarios.updateOne({ nome: "João" }, { $inc: { idade: 1 } });
+
+// Usando o operador $rename para renomear um campo existente
+db.usuarios.updateOne({ nome: "João" }, { $rename: { "endereco.rua": "endereco.nomeRua" } });
+
+// Usando o operador $unset para remover um campo específico de um documento
+db.usuarios.updateOne({ nome: "João" }, { $unset: { endereco: "" } });
+
+// Delete
+// Usando o método deleteOne() para excluir o primeiro documento que corresponde ao filtro especificado
+db.usuarios.deleteOne({ nome: "João" });
+
+// Usando o método deleteMany() para excluir todos os documentos que correspondem ao filtro especificado
+db.usuarios.deleteMany({ cidade: "São Paulo" });
+
+
+// Operadores Lógicos
+db.usuarios.find({ $and: [{ idade: { $gte: 18 } }, { cidade: "São Paulo" }] });
+
+db.usuarios.find({ $or: [{ idade: { $lt: 18 } }, { cidade: "Rio de Janeiro" }] });
+
+db.usuarios.find({ idade: { $not: { $eq: 25 } } });
+
+// Operadores de Comparação
+db.usuarios.find({ idade: { $eq: 25 } });
+
+db.usuarios.find({ idade: { $ne: 30 } });
+
+db.usuarios.find({ idade: { $gt: 30 } });
+
+db.usuarios.find({ idade: { $gte: 30 } });
+
+db.usuarios.find({ idade: { $lt: 30 } });
+
+db.usuarios.find({ idade: { $lte: 30 } });
+
+db.usuarios.find({ cidade: { $in: ["São Paulo", "Rio de Janeiro"] } });
+
+db.usuarios.find({ cidade: { $nin: ["São Paulo", "Rio de Janeiro"] } });
+
+
+// Projeção
+db.usuarios.find({}, { nome: 1, idade: 1 })
+
+// Ordenação
+db.usuarios.find().sort({ idade: 1 })
+// Limitação
+db.usuarios.find().limit(10)
+// Paginação
+db.usuarios.find().skip(10).limit(5)
